baf5150e85640b1f72d35a59912aa27997f1ad20
Andie Lee <andie.d.lee@vanderbilt.edu>
2020-12-01 22:46:53 -0500

    first commit, pushbutton

--- client/src/home/home.js
+++ client/src/home/home.js
@@ -1,17 +1,29 @@
-import React from 'react'
-import Row from 'react-bootstrap/Row'
-import Container from 'react-bootstrap/Container'
-import Button from 'react-bootstrap/Button'
-import Col from 'react-bootstrap/Col'
-import StockCard from '../components/stock-card/stockCard'
+import React from "react";
+import Row from "react-bootstrap/Row";
+import Container from "react-bootstrap/Container";
+import Button from "react-bootstrap/Button";
+import Col from "react-bootstrap/Col";
+import StockCard from "../components/stock-card/stockCard";
 
 function Home() {
   return (
     <Container fluid>
       Stock Visualizer
-      <Row ><Col md={{ span: 4, offset: 7 }}><Button>Add Porfolio</Button></Col></Row>
+      <Row>
+        <Col md={{ span: 4, offset: 7 }}>
+          <Button>
+            <img
+              src=".\Add_Button.png"
+              alt="Add Portfolio IMG"
+              width="75"
+              height="75"
+            />
+            <div>Add Portfolio</div>
+          </Button>
+        </Col>
+      </Row>
       <Row className="justify-content-md-center" xs={2} md={4} lg={6}>
-          <StockCard/>
+        <StockCard />
       </Row>
     </Container>
   );
